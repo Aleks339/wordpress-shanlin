00367e730ec4fa43e4aa71b99b90a048037fe9ab
Aleks339 <annamuljar9@gmail.com>
2018-03-24 20:36:15 +0300

    initial commit

---
+++ header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Учитель китайского Фу Шаньлинь</title>
+
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">
+        <span class="d-block d-lg-none">Start Bootstrap</span>
+        <span class="d-none d-lg-block">
+          <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/shanlin.jpg" alt="">
+        </span>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="index.php">Обо мне</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="experience.php">Мой опыт</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="education.php">Моё образование</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="skills.php">Мои навыки</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="contacts.php">Контакты</a>
+          </li>
+
+        </ul>
+      </div>
+    </nav>
